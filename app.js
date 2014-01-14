4e9f5cee6fc51c0a40cb6355ea60c7a058790281
Andrew Larose <werdnalarose@gmail.com>
2014-01-14 12:02:22 -0500

    Refactored backend and most of frontend

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var express = require('express'),
+	app = express(),
 	path = require('path'),
 	player = require('./lib/player.js'),
-	app = express(),
-	server = require('http').createServer(app);
+	videos = require('./lib/videos.js');
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
-app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
@@ -23,4 +22,17 @@ app.post('/', function(req, res) {
 });
 
 console.log('Listening on port ' + app.get('port'));
-server.listen(app.get('port'));
+var io = require('socket.io').listen(app.listen(app.get('port')));
+
+io.sockets.on('connection', function(socket) {
+	socket.on('new', function(data) {
+		// Once the video has been added to the list, emit the updated list to all connected
+		video.addVideo(data.yturl, function() {
+			io.sockets.emit('update', videos.videoList);
+		})
+	});
+
+	socket.on('control', function(data) {
+
+	});
+});
